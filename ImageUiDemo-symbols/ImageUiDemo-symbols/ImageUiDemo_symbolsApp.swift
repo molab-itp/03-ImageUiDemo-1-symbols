428d240a512e98c4d0ca7e2f715d4f5ab96f8f75
John Henry Thompson <78864877+jht9629-nyu@users.noreply.github.com>
2023-09-14 16:07:54 -0400

    ImageUiDemo-symbols

---
+++ ImageUiDemo-symbols/ImageUiDemo-symbols/ImageUiDemo_symbolsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ImageUiDemo_symbolsApp.swift
+//  ImageUiDemo-symbols
+//
+//  Created by jht2 on 9/14/23.
+//
+
+import SwiftUI
+
+@main
+struct ImageUiDemo_symbolsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            Page1()
+        }
+    }
+}
